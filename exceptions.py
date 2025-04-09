df48caf3bdd3d75ba88b73116201bdea56bb5533
Keerthika <141198736+keerthika-2205@users.noreply.github.com>
2025-04-09 22:54:19 +0530

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+class InvalidPetAgeException(Exception):
+    pass
+
+class NullReferenceException(Exception):
+    pass
+
+class InsufficientFundsException(Exception):
+    pass
+
+class FileHandlingException(Exception):
+    pass
+
+class AdoptionException(Exception):
+    pass
